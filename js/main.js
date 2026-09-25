345bc8573675ddc7763e43afac997ba8d3147fb6
kgdee <kgdee7603@gmail.com>
2026-09-25 21:19:59 +0800

    commit

--- js/main.js
+++ js/main.js
@@ -168,7 +168,7 @@ function getItemIcon(item) {
     if (!isValidUrl(item.url)) return icon;
 
     const newUrl = new URL(item.url);
-    if (newUrl.hostname === "cipher8800.github.io" || newUrl.hostname === "fenixrose84.github.io") {
+    if (newUrl.hostname === "kgdee.github.io" || newUrl.hostname === "fenixrose84.github.io") {
       return `${item.url}/favicon.png`;
     } else if (!newUrl.hostname || newUrl.hostname === "127.0.0.1") {
       return icon;
